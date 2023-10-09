323d07512a820d265e9f572c844541e794dbe382
personal <surajitsarkarofficial@gmail.com>
2023-10-09 18:40:38 +0530

    Added a example of Tuples

---
+++ tuples/tuples.js
@@ -0,0 +1,6 @@
+var studentData;
+studentData = [1, "Suro"];
+studentData.push(2, "Googlie");
+studentData.forEach(function (data) {
+    console.log(data);
+});
---
+++ tuples/tuples.ts
@@ -0,0 +1,7 @@
+let studentData : [number,string];
+studentData=[1,"Suro"];
+studentData.push(2,"Googlie");
+
+studentData.forEach((data)=>{
+    console.log(data);
+})
