19f706fd175d79926104150f2e305d671b4385d3
personal <surajitsarkarofficial@gmail.com>
2023-10-16 11:50:19 +0530

    Union types

---
+++ union/UnionType.js
@@ -0,0 +1,7 @@
+var val;
+val = 50;
+console.log("The value is ".concat(val));
+val = "Hello from Union type";
+console.log("The value is ".concat(val));
+val = true;
+console.log("The value is ".concat(val));
---
+++ union/UnionType.ts
@@ -0,0 +1,8 @@
+let val:number|string|boolean
+
+val=50;
+console.log(`The value is ${val}`)
+val="Hello from Union type";
+console.log(`The value is ${val}`)
+val=true;
+console.log(`The value is ${val}`)
