ca971221d0f2777fc9bb0a8233ad963cb34f41ba
Surajit Sarkar <suro@Surajits-MBP-2.bbrouter>
2023-10-26 17:35:34 +0530

    practiced tuples

--- tuples/tuples.js
+++ tuples/tuples.js
@@ -4,3 +4,25 @@ studentData.push(2, "Googlie");
 studentData.forEach(function (data) {
     console.log(data);
 });
+function display(inputTuple) {
+    console.log("Printing tuple elements");
+    for (var i = 0; i < inputTuple.length; i++) {
+        console.log(inputTuple[i]);
+    }
+    console.log("---------------");
+}
+//declaring tuple
+var empTuple = [101, 'Suro', 'Pune'];
+//adding more elements to a tuple
+empTuple.push('Married');
+empTuple.push('QA');
+//passing tuple to a function
+display(empTuple);
+//removing element from tuple
+empTuple.pop();
+display(empTuple);
+//updating tuple
+empTuple[0] = 200;
+display(empTuple);
+//accessing tuple by index
+console.log('Tuple element at index 1 is ' + empTuple[1]);
--- tuples/tuples.ts
+++ tuples/tuples.ts
@@ -5,3 +5,34 @@ studentData.push(2,"Googlie");
 studentData.forEach((data)=>{
     console.log(data);
 })
+
+
+function display(inputTuple:any[])
+{
+    console.log("Printing tuple elements");
+    for(let i=0;i<inputTuple.length;i++)
+    {
+        console.log(inputTuple[i])
+    }
+    console.log("---------------");
+}
+
+//declaring tuple
+let empTuple=[101,'Suro','Pune'];
+//adding more elements to a tuple
+empTuple.push('Married');
+empTuple.push('QA')
+
+//passing tuple to a function
+display(empTuple);
+
+//removing element from tuple
+empTuple.pop()
+display(empTuple);
+
+//updating tuple
+empTuple[0]=200;
+display(empTuple);
+
+//accessing tuple by index
+console.log('Tuple element at index 1 is '+empTuple[1])
